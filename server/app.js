686008329a419758f308d9400231a77d276ed5e0
Eric Wan <ericwan957@gmail.com>
2025-04-11 05:46:43 -0400

    Fix issue with login
    Authorization header was being checked on every page. Now the check is
    disabled on the login/register page.

--- server/app.js
+++ server/app.js
@@ -18,31 +18,31 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // Middleware to authenticate and attach user info
-app.use((req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (authHeader) {
-      const token = authHeader.split(' ')[1]; // Extract the token
-      try {
-        const user = jwt.verify(token, process.env.JWT_SECRET); // Decode the token
-        req.user = { email: user.email, name: user.name }; // Attach the user's name and email to req.user
-      } catch (err) {
-        console.error('Invalid token');
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-    } else {
-      return res.status(401).json({ error: 'Authorization header missing' });
+function needsAuth(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1]; // Extract the token
+    try {
+      const user = jwt.verify(token, process.env.JWT_SECRET); // Decode the token
+      req.user = { email: user.email, name: user.name }; // Attach the user's name and email to req.user
+    } catch (err) {
+      console.error('Invalid token');
+      return res.status(401).json({ error: 'Unauthorized' });
     }
-    next();
-});
+  } else {
+    return res.status(401).json({ error: 'Authorization header missing' });
+  }
+  next();
+}
 
 // API
 app.use('/api/auth', authRoutes);
-app.use('/api/portfolios', portfolios);
-app.use('/api/stocklists', stocklists);
-app.use('/api/friends', friends);
-app.use('/api/reviews', reviews);
-app.use('/api/predictions', predictions);
-app.use('/api/stocks', stocks);
+app.use('/api/portfolios', needsAuth, portfolios);
+app.use('/api/stocklists', needsAuth, stocklists);
+app.use('/api/friends', needsAuth, friends);
+app.use('/api/reviews', needsAuth, reviews);
+app.use('/api/predictions', needsAuth, predictions);
+app.use('/api/stocks', needsAuth, stocks);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
